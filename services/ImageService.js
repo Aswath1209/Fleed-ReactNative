1d8989319d8ce4a82ca38289c6a04019b1fb0f53
Aswath <aswathnarayanan39@gmail.com>
2026-01-16 12:08:01 +0530

    Fix: Convert default_user and fleed_logo from disguised JPEGs to true PNGs

--- services/ImageService.js
+++ services/ImageService.js
@@ -8,7 +8,7 @@ export const getUserImageSrc = imagePath => {
     if (imagePath) {
         return getSupabaseFileUrl(imagePath);
     } else {
-        return require('../assets/images/defaultUser.png');
+        return require('../assets/images/default_user.png');
     }
 }
 
